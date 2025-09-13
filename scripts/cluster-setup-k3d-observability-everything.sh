c5d773e101ff378956905b6e2a1a7bdb81063588
Tom Dean <tdean@d2iq.com>
2025-09-12 22:32:25 -0500

    More work on observability.

--- scripts/cluster-setup-k3d-observability-everything.sh
+++ scripts/cluster-setup-k3d-observability-everything.sh
@@ -86,7 +86,7 @@ echo "This may take a few minutes."
 # Add Helm repos
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
-helm repo add unpoller https://unpoller.github.io/helm-charts
+helm repo add unpoller https://unpoller.github.io/helm-chart
 helm repo update
 
 # Install kube-prometheus-stack
