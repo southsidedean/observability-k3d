52d5292d091ca11ded069ea8311e0d8ff5ca48c4
Tom Dean <tom@dean33.com>
2025-09-29 16:47:16 -0500

    Updates to installed versions of kagent, kgateway and k3s images.

--- vars.sh
+++ vars.sh
@@ -14,13 +14,13 @@ export CLUSTER_NETWORK=observability-network
 export PERSISTENT_DATA_PATH="/media/content/observability-k3d"
 export MONITORING_NAMESPACE=monitoring
 export KAGENT_NAMESPACE=kagent
-export KAGENT_VERSION=0.6.12
+export KAGENT_VERSION=0.6.16
 export OPENAI_API_KEY=""
-export KGATEWAY_VERSION="2.0.4"
+export KGATEWAY_VERSION="2.1.0"
 export KGATEWAY_NAMESPACE=kgateway-system
 export GATEWAY_API_VERSION="v1.3.0"
 export GRAFANA_ADMIN_PASSWORD="prom-operator"
-export K3S_VERSION="rancher/k3s:v1.31.7-k3s1"
+export K3S_VERSION="rancher/k3s:v1.33.5-k3s"
 
 # --- UniFi Poller Settings ---
 export UNIFI_CONTROLLER_URL="https://unifi.yourdomain.com" # CHANGE THIS to your UniFi controller URL
