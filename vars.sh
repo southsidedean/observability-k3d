231107aa543daba59e3a5d40add8c4c612cd3c4c
Tom Dean <tdean@d2iq.com>
2026-02-23 19:08:33 -0600

    Updated kagent, kgateway, Gateway API and k8s versions in varsh.sh.

--- vars.sh
+++ vars.sh
@@ -14,13 +14,13 @@ export CLUSTER_NETWORK=observability-network
 export PERSISTENT_DATA_PATH="/media/content/observability-k3d"
 export MONITORING_NAMESPACE=monitoring
 export KAGENT_NAMESPACE=kagent
-export KAGENT_VERSION=0.6.16
+export KAGENT_VERSION=0.7.17
 export OPENAI_API_KEY=""
-export KGATEWAY_VERSION="2.1.0-main"
+export KGATEWAY_VERSION="2.2.1"
 export KGATEWAY_NAMESPACE=kgateway-system
-export GATEWAY_API_VERSION="v1.3.0"
+export GATEWAY_API_VERSION="v1.4.1"
 export GRAFANA_ADMIN_PASSWORD="prom-operator"
-export K3S_VERSION="rancher/k3s:v1.33.5-k3s1"
+export K3S_VERSION="rancher/k3s:v1.35.1-k3s1"
 
 # --- UniFi Poller Settings ---
 export UNIFI_CONTROLLER_URL="https://unifi.yourdomain.com" # CHANGE THIS to your UniFi controller URL
