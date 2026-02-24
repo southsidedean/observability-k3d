5008bf35e1f7fadfd9495fe747adffbe3afd5f06
Tom Dean <tdean@d2iq.com>
2026-02-23 19:09:22 -0600

    Updated persistent data path in vars.sh.

--- vars.sh
+++ vars.sh
@@ -11,7 +11,7 @@
 export CLUSTER_NAME=observability-cluster
 export KUBECTX_NAME=observability-cluster
 export CLUSTER_NETWORK=observability-network
-export PERSISTENT_DATA_PATH="/media/content/observability-k3d"
+export PERSISTENT_DATA_PATH=""
 export MONITORING_NAMESPACE=monitoring
 export KAGENT_NAMESPACE=kagent
 export KAGENT_VERSION=0.7.17
